517bbe564f612003be3d3eafd0ac1f3e81090ae4
Anjoee <82038957+Theesawan39@users.noreply.github.com>
2021-05-14 10:23:07 +0700

    l

---
+++ exercise_def_theesawans.py
@@ -0,0 +1,11 @@
+
+def addNumber(x, y):
+    print(x + y)
+def addNumber1(x, y):
+    print(x - y)
+def addNumber2(x, y):
+    print(x * y)
+def addNumber3(x,y):
+    print(x / y)
+
+print(addNumber3(90,7))
